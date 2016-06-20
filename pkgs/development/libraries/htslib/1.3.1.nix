93e08602c93f0df7159fc0bf61522606f63dd07f
David J. H. Shih <djh.shih@gmail.com>
2016-06-20 17:51:42 -0400

    updated: htslib-1.3.1

---
+++ pkgs/development/libraries/htslib/1.3.1.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetchurl, zlib, perl }:
+
+stdenv.mkDerivation rec {
+  version = "1.3.1";
+  name = "htslib-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/samtools/htslib/releases/download/${version}/${name}.tar.bz2";
+    sha256 = "1rja282fwdc25ql6izkhdyh8ppw8x2fs0w0js78zgkmqjlikmma9";
+  };
+
+  buildInputs = [ zlib perl ];
+
+  doCheck = true;
+
+  meta = with stdenv.lib; {
+    description = "A C library for reading/writing high-throughput sequencing data";
+    license     = licenses.free;
+    platforms   = platforms.unix;
+    homepage    = "http://bedtools.readthedocs.org"; 
+  };
+}
